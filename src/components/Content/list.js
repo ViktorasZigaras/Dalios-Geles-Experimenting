da8cbfe2a8c6029af0596e03f6af3681add92add
ViktorasZigaras <44396727+ViktorasZigaras@users.noreply.github.com>
2020-03-29 16:11:53 +0300

    fix

--- src/components/Content/list.js
+++ src/components/Content/list.js
@@ -53,8 +53,10 @@ const json = ( props ) => {
                     let html = [ ]
                     category.forEach( 
                         ( item ) => {
-                            url1 = `../pictures/daylily/${ item.img }-1.jpg`
-                            url2 = `../pictures/daylily/${ item.img }-2.jpg`
+                            url1 = `../assets/${ item.img }-1.jpg`
+                            url2 = `../assets/${ item.img }-2.jpg`
+                            // url1 = `../pictures/daylily/${ item.img }-1.jpg`
+                            // url2 = `../pictures/daylily/${ item.img }-2.jpg`
                             html.push(
                                 <div className="list-item">
                                     
